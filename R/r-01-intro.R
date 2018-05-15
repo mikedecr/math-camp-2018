9e09ee2c1b86cf89a5d4f6c6987d23aac8842715
Michael DeCrescenzo <mgdecrescenzo@gmail.com>
2018-05-15 09:15:16 -0500

    intro slides (testing xaringan)

---
+++ R/r-01-intro.R
@@ -0,0 +1,41 @@
+# --------------------------------------
+# Math Camp 2018:
+# Basics of R
+# --------------------------------------
+
+
+# --- R intro -----------------
+
+# R is a REPL, executes commands one at a time
+# console entry
+# running from a source file
+
+# arithmetic
+# order of operations
+
+
+# --- Objects -----------------
+
+# assignment
+# printing
+# use as variables
+
+
+
+# R is a REPL (read, evaluate, print, loop).
+# This means that it executes commands and returns results *one at a time*
+
+# Results are printed in the CONSOLE.
+# The console can be displayed...
+# - in the R application (R.app on Mac, R.exe on Windows)
+# - in the Rstudio application (on Mac or Windows)
+# - or as a terminal program
+
+# to demonstrate, R can perform simple math.
+# Type the following commands directly into the console
+
+1 + 1
+
+2 * 10
+
+1 / 2
