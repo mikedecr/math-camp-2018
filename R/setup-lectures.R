170c3c20652662822624f75b37aff604c275545f
Michael DeCrescenzo <mgdecrescenzo@gmail.com>
2018-07-19 22:58:02 -0500

    Pre-calc lecture draft

--- R/setup-lectures.R
+++ R/setup-lectures.R
@@ -2,7 +2,7 @@ library("here")
 library("magrittr")
 library("tidyverse")
 library("ggplot2")
-
+library("latex2exp")
 
 #  custom ggtheme, based on theme_mgd()
 source(here("R/theme_mathcamp.R"))
@@ -12,24 +12,24 @@ theme_set(theme_mathcamp()) # set default theme
 # suppress version on html output folders
 options(htmltools.dir.version = FALSE)
 
+
 # colors to match the slides and graphics
 primary <- "#9b0000"
 secondary <- "#0479a8"
 
+
+
 # Xaringan settings
+
 library("xaringan")
 library("xaringanthemer")
-# mono_accent(base_color = primary)
+
 duo_accent(primary_color = primary, 
            secondary_color = secondary, 
            header_font_google = google_font("Vollkorn"), 
-           # text_font_google   = google_font("Source Sans Pro"), 
-           text_font_google   = google_font("Roboto"), 
-           code_font_google   = google_font("Roboto Mono"),
-           # code_font_google   = google_font("CamingoCode"),
-           # text_bold_color = "#000000",
-           # code_inline_color = "#000000",
-           code_inline_background_color = "#F5F5F5",
+           text_font_google = google_font("Roboto"), 
+           code_font_google = google_font("Roboto Mono"),
+           code_inline_background_color    = "#F5F5F5",
            table_row_even_background_color = "white",
            extra_css = 
              list(".remark-slide-number" = list("display" = "none")))
@@ -40,7 +40,8 @@ duo_accent(primary_color = primary,
 # cache everything
 knitr::opts_chunk$set(eval = TRUE, echo = FALSE, 
                       warning = FALSE, message = FALSE,
-                      cache = TRUE)
+                      cache = TRUE,
+                      fig.align = "center", dpi = 100)
 
 
 
